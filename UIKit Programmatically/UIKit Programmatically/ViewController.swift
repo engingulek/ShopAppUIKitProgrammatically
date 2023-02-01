53000f5e94690488917b72b58dbcb243ab2e8e78
Engin Gülek <engingulek0@gmail.com>
2023-02-01 19:34:20 +0300

    UILabel

--- UIKit Programmatically/UIKit Programmatically/ViewController.swift
+++ UIKit Programmatically/UIKit Programmatically/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Title Label"
+        label.textColor = UIColor.red
+        label.font = UIFont.systemFont(ofSize: 30)
+        return label
+    }()
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+    
+        view.addSubview(titleLabel)
+        
     }
-
-
+    
+    
+    func configure(){
+        // MARK: - Title UILabel
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
 }
 
