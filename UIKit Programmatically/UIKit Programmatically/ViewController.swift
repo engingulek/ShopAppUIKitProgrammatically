250cf513ff2e69024878706cb1287cb2341ca983
Engin Gülek <engingulek0@gmail.com>
2023-02-01 17:23:39 +0300

    First Commit

---
+++ UIKit Programmatically/UIKit Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Programmatically
+//
+//  Created by engin gülek on 1.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
